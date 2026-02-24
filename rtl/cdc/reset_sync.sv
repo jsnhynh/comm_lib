6a8b4872896348484414cb336169b903cd8058cb
Jason H <47314658+jsnhynh@users.noreply.github.com>
2026-02-23 21:54:21 -0800

    reset_sync

--- rtl/cdc/reset_sync.sv
+++ rtl/cdc/reset_sync.sv
@@ -6,3 +6,37 @@
     async_rst -> FF1 -> FF2 -> sync_rst
 */
 
+module reset_sync0 (
+    input  logic clk,
+    input  logic rst_n,
+    output logic rst_n_sync
+);
+    (* ASYNC_REG = "TRUE" *) logic ff1, ff2;
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            ff1 <= '0;
+            ff2 <= '0;
+        end else begin
+            ff1 <= '1;
+            ff2 <= ff1;
+        end
+    end
+    assign rst_n_sync = ff2;
+endmodule
+
+module reset_sync1 (
+    input  logic clk,
+    input  logic rst,
+    output logic rst_sync
+);
+    (* ASYNC_REG = "TRUE" *) logic ff1, ff2;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            ff1 <= '1;
+            ff2 <= '1;
+        end else begin
+            ff1 <= '0;
+            ff2 <= ff1;
+        end
+    end
+endmodule
